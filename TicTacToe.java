95e8ad1c60360afa08cbc807b882f67f3666a471
Wyatt G <72100134+wgonzale@users.noreply.github.com>
2020-10-07 21:07:05 -0400

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -44,29 +44,103 @@ public class TicTacToeGame {
 
 	static int winOrTie() {
 	    
-		int winTotal = 0;
+		int colTotal = 0; // need to implement a colTotal to check columns
+		int rowTotal = 0;
+		int diagTotal = 0;
+		int reverseDiagTotal = 0;
+		
+		
 		for(int row=0;row<gameboard.length;row++) {
-		   winTotal =0;
-			for(int col=0;col<gameboard[row].length;col++) {
-			   if(gameboard[row][col] == 0) {
-				   return -2;
-		  }
-			   winTotal = winTotal + gameboard[row][col];
-	   }
-			   if(winTotal==gameboard.length){
-				   return CROSS;
-	 
-		   }
-			   else if(winTotal==gameboard.length * -1) {
-				   return NOUGHT;
-				   
+		   rowTotal =0;
+		   	colTotal = 0;
+			for(int col=0;col<gameboard[row].length;col++) { //Navigates the column first checking if X, or 0 has won
+			   
+			   rowTotal = rowTotal + gameboard[row][col]; //Stores value win total to see if it equals gameboard length to determine winner based on rows and columns
+			   colTotal = colTotal + gameboard[col][row]; //Stores value 
+			   
+					   if(row == col) {
+				   diagTotal = diagTotal + gameboard[row][col];
 			   }
-			  		 
-			  
-	   }    		
+               if(row + col == gameboard.length -1) {
+            	   reverseDiagTotal = reverseDiagTotal + gameboard[row][col];
+               }
+			}
+			   if(rowTotal==gameboard.length){ 
+				   return CROSS;
+			   }
+			   else if(rowTotal==gameboard.length * -1) {
+				   return NOUGHT;  
+			   }
+			   else if(colTotal == gameboard.length) {
+				   return CROSS;
+			   }
+			   else if(colTotal == gameboard.length * -1) {
+				   return NOUGHT;
+			   }
+			   else if(diagTotal == gameboard.length) {
+				   return CROSS;
+			   }
+			   else if(diagTotal == gameboard.length * -1) {
+				   return NOUGHT;
+			   }
+			   else if(reverseDiagTotal == gameboard.length) {
+				   return CROSS;
+			   }
+			   
+			   else if(reverseDiagTotal == gameboard.length * -1) {
+				   return NOUGHT;
+			   }
+			  	 
+		}	 
+		
+		for(int row=0;row<gameboard.length;row++) {
+				for(int col=0;col<gameboard[row].length;col++) { //Navigates the column first checking if X, or 0 has won
+				   if(gameboard[row][col] == 0) {
+					   return -2;
+				   }
+				}
+		}   
+				return 0;  		
+				
+		    
 	}
 	
 	public static void main(String[] args) {
+		
+		createBoard(3,3);
+		int turn = 0;
+		int playerVal;
+		int outcome;
+		java.util.Scanner scan = new java.util.Scanner(System.in);
+		do {
+		    displayBoard();
+		    playerVal = (turn % 2 == 0)? NOUGHT : CROSS;
+		    if (playerVal == NOUGHT)
+		        System.out.println ("\n—O's turn—");
+		    else System.out.println("\n—X's turn—" );
+		    System.out.print("Enter row and column:" );
+		    try {
+		        set(playerVal, scan.nextInt(), scan.nextInt());
+		    } catch (Exception ex)
+		    {System.err.println(ex); turn--;}
+		    
+		    turn ++;
+		    outcome = winOrTie();
+		} while ( outcome == -2 );
+
+		displayBoard();
+
+		switch (outcome) {
+		case NOUGHT:
+		    System.out.println("O wins!");
+		    break;
+		case CROSS:
+		    System.out.println("X wins!");
+		    break;
+		case 0:
+		    System.out.println("Tie.");
+		    break;
+		}
 		
 		
 
