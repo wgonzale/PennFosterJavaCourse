58e1423010e01f18a90d0753314dcfeb5b4253bf
Wyatt G <72100134+wgonzale@users.noreply.github.com>
2020-10-11 20:13:35 -0400

    Create FileManager.java

---
+++ BoardGameTester/games.utilities/FileManager.java
@@ -0,0 +1,47 @@
+package games.utilities;
+import java.io.*;
+
+public class FileManager{
+
+	public static void writeToFile(String saveState, String fileName) throws java.io.IOException {
+		
+		try(FileWriter myWriter = new FileWriter(fileName);){
+			myWriter.write(saveState);
+		}
+		catch(IOException e) {
+			System.out.println("An error occurred");
+		}
+	}
+
+	public static void writeToFileAsync(String saveState, String fileName) throws java.io.IOException {
+	
+		class myThread implements Runnable{
+			Thread mt;
+			myThread(){
+				mt = new Thread();
+				mt.start();
+			}
+			
+			public void run() {
+				try {
+					FileWriter myWriter = new FileWriter(fileName);
+					myWriter.write(saveState);
+					myWriter.close();
+				}
+				catch(IOException e) {
+					System.out.println("An error occurred");
+				}
+			}
+			
+			
+		}
+	
+		
+		
+		
+	}
+
+
+
+
+}
